a4f9a8fbed2c4325482795e9420919a994176f19
Flaco013 <alexisgasga7@gmail.com>
2023-12-02 11:24:06 -0700

    Updates on CrearProducto with alerts

--- resources/js/components/CrearProducto.jsx
+++ resources/js/components/CrearProducto.jsx
@@ -1,27 +1,71 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-
+import Swal from 'sweetalert2';
 
 const endpoint = "http://localhost:8000/api/inventarios";
 
 const ModalInv = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
+    // States
     const [name, setName] = useState("");
     const [tipoProducto, setTipoProducto] = useState("");
     const [unidadMedida, setUnidadMedida] = useState("");
-    const [cantidad, setCantidad] = useState(0);
-    const [cantidadMinima, setCantidadMinima] = useState(0);
+    const [cantidad, setCantidad] = useState(1);
+    const [cantidadMinima, setCantidadMinima] = useState(1);
+
+    // Validation
+    const [isNameValid, setIsNameValid] = useState(false);
+    const [isCantidadValid, setIsCantidadValid] = useState(true);
+    const [isCantidadMinimaValid, setIsCantidadMinimaValid] = useState(true);
+
+    // Focus check
+    const [isNameFocused, setIsNameFocused] = useState(false);
+    const [isCantidadFocused, setIsCantidadFocused] = useState(false);
+    const [isCantidadMinimaFocused, setIsCantidadMinimaFocused] = useState(false);
+
+    //List of regex
+    const RegexName = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s-]+$/;
+    const RegexPositiveNumber = /^[1-9]+[0-9]*$/;
+
+    // Functions
+	function onSetName(value) {
+        setName(value);
+        setIsNameValid(RegexName.test(value) && value.length > 0);       
+    }
+
+    function onSetCantidad(value) {
+        setCantidad(value);
+        setIsCantidadValid(value > 0);       
+    }
+
+    function onSetCantidadMinima(value) {
+        setCantidadMinima(value);
+        setIsCantidadMinimaValid(value > 0);       
+    }
+
 
     const create = async (e) => {
         e.preventDefault();
-
+    
         console.log("Sending data:", { nombre: name, tipo: tipoProducto, unidad_medida: unidadMedida, cantidad: cantidad, cantidad_minima: cantidadMinima});
-
-        await axios.post(endpoint, { nombre: name, tipo: tipoProducto, unidad_medida: unidadMedida, cantidad: cantidad, cantidad_minima: cantidadMinima});
-        onClose();
-        window.location.reload();
+        
+        try {
+            await axios.post(endpoint, { nombre: name, tipo: tipoProducto, unidad_medida: unidadMedida, cantidad: cantidad, cantidad_minima: cantidadMinima});
+            onClose();
+            window.location.reload();
+        } catch (error) {
+            console.error("Error:", error);
+    
+            // alert
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Verifique que la entrada sea única y los datos sean correctos.',
+            });
+        }
     }
+    
 
     return (
         <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex">
@@ -33,18 +77,24 @@ const ModalInv = ({ isOpen, onClose }) => {
                     <h2 className='text-center mb-5'>Productos</h2>
                     <div>
 
-                        <label htmlFor="nuevoElemento" className="block mb-2 text-sm font-medium text-gray-900">
+                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">
                             Nombre del Producto
                         </label>
                         <input
                             value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            onChange={(e) => onSetName(e.target.value)}
+                            onFocus={() => setIsNameFocused(true)}
                             type="text"
-                            id="nuevoElemento"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            id="name"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-2"
                             placeholder="Ingresar"
                             required
                         />
+                        {isNameValid || !isNameFocused  ? null : (
+                        <div className="text-red-500 text-sm mt-1 mb-2">
+                            Ingrese nombre de producto válido y con mayor a 3 caracteres.
+                            </div>
+                            )}
 
                         <div className="col-span-2 sm:col-span-1">
                             <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Tipo de Producto</label>
@@ -52,7 +102,7 @@ const ModalInv = ({ isOpen, onClose }) => {
                                 value={tipoProducto}
                                 onChange={(e) => setTipoProducto(e.target.value)}
                                 id="category" 
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-2"
                              >
                                 <option className='text-black'>Seleccionar</option>
                                 <option className='text-black' value="Carne">Carne</option>
@@ -68,12 +118,12 @@ const ModalInv = ({ isOpen, onClose }) => {
                         </div>
 
                         <div className="col-span-2 sm:col-span-1">
-                            <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Unidad de Medida</label>
+                            <label htmlFor="unidad" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Unidad de Medida</label>
                             <select
                                 value={unidadMedida}
                                 onChange={(e) => setUnidadMedida(e.target.value)} 
-                                id="category" 
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                id="unidad" 
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-2"
                              >
                                 <option className='text-black'>Seleccionar</option>
                                 <option className='text-black' value="Kilogramos">Kilogramos</option>
@@ -83,31 +133,43 @@ const ModalInv = ({ isOpen, onClose }) => {
                             </select>
                         </div>
 
-                        <label htmlFor="nuevoElemento" className="block mb-2 text-sm font-medium text-gray-900">
+                        <label htmlFor="cantidad" className="block mb-2 text-sm font-medium text-gray-900">
                             Cantidad
                         </label>
                         <input
                             value={cantidad}
-                            onChange={(e) => setCantidad(e.target.value)}
+                            onChange={(e) => onSetCantidad(e.target.value)}
+                            onFocus={() => setIsCantidadFocused(true)}
                             type="number"
-                            id="nuevoElemento"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            id="cantidad"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-2"
                             placeholder="Ingresar"
                             required
                         />
+                        {isCantidadValid || !isCantidadFocused ? null : (
+                        <div className="text-red-500 text-sm mt-1 mb-2">
+                            Ingrese número mayor a 0
+                            </div>
+                            )}
 
-                        <label htmlFor="nuevoElemento" className="block mb-2 text-sm font-medium text-gray-900">
+                        <label htmlFor="cantidadMinima" className="block mb-2 text-sm font-medium text-gray-900">
                             Cantidad minima
                         </label>
                         <input
                             value={cantidadMinima}
-                            onChange={(e) => setCantidadMinima(e.target.value)}
+                            onChange={(e) => onSetCantidadMinima(e.target.value)}
+                            onFocus={() => setIsCantidadMinimaFocused(true)}
                             type="number"
-                            id="nuevoElemento"
+                            id="cantidadMinima"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             placeholder="Ingresar"
                             required
                         />
+                        {isCantidadMinimaValid || !isCantidadMinimaFocused  ? null : (
+                        <div className="text-red-500 text-sm mt-1 mb-2">
+                            Ingrese número mayor a 0
+                            </div>
+                            )}
 
                         <div className="flex justify-center mt-4">
                             <button
