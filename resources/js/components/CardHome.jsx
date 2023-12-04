a4d13167d449e11400ce1387b1aa6ef894fd6cd9
Martín Castro <mcastro_20@alu.uabcs.mx>
2023-12-04 12:40:55 -0700

    Add: Funcion de bienvenida en Home, y cards funcionales (rutas)

--- resources/js/components/CardHome.jsx
+++ resources/js/components/CardHome.jsx
@@ -1,126 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function CardHome({ name, comidas, description,description2, option, price, conxt, conxt2,boton, checkpoint,  index=1 }) {
-  const cardColors = ["bg-red-500", "bg-green-500", "bg-yellow-500"];
-  const cardIndex = index;
-  const cardColorClass = cardColors[cardIndex % cardColors.length];
-  let descriptionColor, descriptionColor2;
-  
-  switch (description) {
-    case 'Agotado':
-      descriptionColor = 'text-red-700';
-      break;
-    case 'Cantidad baja':
-      descriptionColor = 'text-yellow-500';
-      break;
-      case 'Entregada':
-        descriptionColor = 'text-green-700';
-        break;
+export default function CardHome({
+    name,
+    comidas,
+    description,
+    conxt,
+    conxt2,
+    index = 1,
+    ruta
+}) {
+    const cardColors = ["bg-red-500", "bg-green-500", "bg-yellow-500"];
+    const cardIndex = index;
+    const cardColorClass = cardColors[cardIndex % cardColors.length];
+    const navigate = useNavigate();
 
-    case 'Disponible':
-      descriptionColor = 'text-green-700';
-      break;
-    default:
-      descriptionColor = 'text-black';
-  }
+    return (
+        <div onClick={() => navigate(`${ruta}`)} className={`${cardColorClass} shadow-md cursor-pointer rounded-2xl mx-4 p-8`}>
+            <div className="flex items-center flex-col">
+                <img
+                    src={comidas}
+                    alt="Sushi de Pollo"
+                    className="w-full max-h-60 object-cover rounded-2xl"
+                />
+                <div className=" mt-3 text-center">
+                    <h2 style={{ fontSize: "3.0rem" }} className="font-black">
+                        {name}
+                    </h2>
+                    <div className="lg:flex items-center">
+                        <a
+                            style={{ fontSize: "1.4rem" }}
+                            className="text-md text-black font-medium"
+                        >
+                            {description}
+                        </a>
+                    </div>
 
-
-  switch (checkpoint) {
-    case 'Cancelada':
-      descriptionColor2 = 'text-red-700';
-      break;
-    case 'Pendiente':
-      descriptionColor2 = 'text-yellow-500';
-      break;
-      case 'Entregada':
-        descriptionColor2 = 'text-green-700';
-        break;
-  }
-
-  const [showAlert, setShowAlert] = useState(false);
-
-  const handleAlert = () => {
-    setShowAlert(true);
-  };
-
-  useEffect(() => {
-    let timeout;
-    if (showAlert) {
-      timeout = setTimeout(() => {
-        setShowAlert(false);
-      }, 2000); 
-    }
-
-
-  return () => {
-    clearTimeout(timeout);
-  };
-}, [showAlert]);
-
- 
-
-  return (
-    
-    <div className={`${cardColorClass} shadow-md rounded-2xl mb-60 mx-4 p-7 flex flex-col max-w-md`}>
-      <div className="flex items-center flex-col">
-        <img
-          src={comidas}
-          alt="Sushi de Pollo"
-          className="w-full max-h-60 object-cover rounded-2xl"
-        />
-        <div className="flex-grow mt-3 text-center">
-          <div className="text-xs text-blue-700 font-medium">{option}</div>
-          
-          <h2 style={{ fontSize: '2.0rem' }} className="font-medium">{name}</h2>
-          <div className="lg:flex items-center">
-            <a style={{ fontSize: '1.4rem' }} className={`mr-1 ${descriptionColor}`}>{description}</a>
-          </div>
-          <div className="lg:flex items-center">
-            <button style={{ fontSize: '1.2rem' }} className={`mr-1 ${descriptionColor}`}>{description2}</button>
-          </div>
-
-          <div className="lg:flex items-center">
-            <button style={{ fontSize: '1.2rem' }} className={`mr-1 ${descriptionColor2}`}>{checkpoint}</button>
-          </div>
-
-
-          <div className="lg:flex items-center">
-            <button style={{ fontSize: '1.2rem' }} className={`mr-1 ${descriptionColor}`}>{price}</button>
-          </div>
-          <div className="text-md text-black font-medium">{conxt}</div>
-          <div className="text-md text-black font-medium">{conxt2}</div>
-          <div className="flex justify-center space-x-3 text-md font-semibold mt-10">
-            
-            
-
-            <button
-          className="rounded-md border hover:bg-blue-300 hover:text-white text-blue-500 border-purple-500 py-1 px-3"
-          type="button"
-          onClick={handleAlert}
-        >
-          {boton}
-        </button>
-
-            
-
-           
-          </div>
-
-          {showAlert && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 -mt-10">
-          <div className="bg-orange-300 border border-gray-200 shadow-md rounded-lg p-6">
-            <p>Esta orden ya esta marcada como Entregada</p>
-          </div>
+                    <div className="text-md text-black font-medium">
+                        {conxt}
+                    </div>
+                    <div className="text-md text-black font-medium">
+                        {conxt2}
+                    </div>
+                </div>
+            </div>
         </div>
-      )}
-
-        </div>
-      </div>
-     
-    </div>
-
-    
-  
-  );
+    );
 }
--- resources/js/pages/Inicio.jsx
+++ resources/js/pages/Inicio.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar'
 import CardHome from '../components/CardHome'
-import Navbar from '../components/Navbar'
 import Layout from '../layouts/Layout';
-export default function Inicio() {
-    return (
-        <Layout>
-            <div className='overflow-scroll'>
 
-                <h1 className='text-white font-bold text-4xl text-center mt-10'>Sushi Bar Pagina Administrativa</h1>
+export default function Inicio() {
+
+    const date = new Date();
+    const hour = date.getHours();
+    let time = '';
+
+    if(hour < 12){
+        time = 'días! 🌤️';
+    }else if(hour >= 12 && hour < 18){
+        time = 'tardes! ☀️';
+    }else{
+        time = 'noches! 🌙';
+    }
+
+    return (
+        
+        <Layout>
+            <div className='overflow-y-auto h-screen'>
+
+                <h1 className='text-white font-black text-6xl text-center mb-10 mt-10'>¡Bienvenido, buenas {time}</h1>
+                <h1 className='text-white font-medium text-3xl text-center mt-3'>Sushi Bar | Página Administrativa</h1>
                 
-              
-                
-                <main className='h-screen mt-10 flex flex-col md:flex-row md:flex-wrap md:justify-center gap-4 -mb-28'>
-                    <CardHome name="Platillos" comidas={"sushiii.jpg"} description="Total de platillos: 20" conxt="Platillos no disponibles: 7  " conxt2="Platillos disponibles: 13  " index={1}/>
-                    <CardHome name="Ordenes" comidas={"rest.png"} description="Total de Ordenes: 12" conxt="Ordenes completadas: 2  " conxt2="Ordenes pendientes: 10  " index={2}/>
-                    <CardHome name="Inventario" comidas={"camaron.jpg"} description="Total de productos: 22  " conxt="Productos agotados: 10" conxt2="Productos disponibles: 12  " index={3}/>
+                <main className='mt-10 flex flex-col md:flex-row md:flex-wrap md:justify-center gap-4'>
+                    <CardHome name="Platillos" comidas={"sushiii.jpg"} description="Total de Platillos: 20" conxt="Platillos no disponibles: 7" conxt2="Platillos disponibles: 13" index={1} ruta="/platillos"/>
+                    <CardHome name="Ordenes" comidas={"rest.png"} description="Total de Ordenes: 12" conxt="Ordenes completadas: 2" conxt2="Ordenes pendientes: 10" index={2} ruta="/ordenes"/>
+                    <CardHome name="Inventario" comidas={"camaron.jpg"} description="Total de Productos: 22  " conxt="Productos agotados: 10" conxt2="Productos disponibles: 12" index={3} ruta="/inventario"/>
                 </main>
 
             </div>
