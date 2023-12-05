5c744f5ee7092863fdc6a99aa5a400e9842961a6
Martín Castro <mcastro_20@alu.uabcs.mx>
2023-12-05 10:37:06 -0700

    Add: Conteo general de registros en cards de Inicio

--- resources/js/pages/Inicio.jsx
+++ resources/js/pages/Inicio.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CardHome from '../components/CardHome'
 import Layout from '../layouts/Layout';
+import axios from 'axios';
+
+const endpoint = 'http://localhost:8000/api';
 
 export default function Inicio() {
+
+    const [totalPlatillos, setTotalPlatillos] = useState(0);
+    const [totalOrdenes, setTotalOrdenes] = useState(0);
+    const [totalProductos, setTotalProductos] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${endpoint}/platillos`)
+        .then(res => {
+            setTotalPlatillos(res.data.data.length);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+        axios.get(`${endpoint}/ordenes`)
+        .then(res => {
+            setTotalOrdenes(res.data.data.length);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+        axios.get(`${endpoint}/inventarios`)
+        .then(res => {
+            setTotalProductos(res.data.data.length);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    } ,[])
+
 
     const date = new Date();
     const hour = date.getHours();
     let time = '';
 
     if(hour < 12){
-        time = 'días! 🌤️';
-    }else if(hour >= 12 && hour < 18){
-        time = 'tardes! ☀️';
+        time = 'buenos días! 🌤️';
+    }else if(hour >= 12 && hour < 19){
+        time = 'buenas tardes! ☀️';
     }else{
-        time = 'noches! 🌙';
+        time = 'buenas noches! 🌙';
     }
 
     return (
@@ -21,13 +55,13 @@ export default function Inicio() {
         <Layout>
             <div className='overflow-y-auto h-screen'>
 
-                <h1 className='text-white font-black text-6xl text-center mb-10 mt-10'>¡Bienvenido, buenas {time}</h1>
+                <h1 className='text-white font-black text-6xl text-center mb-10 mt-6'>¡Bienvenido, {time}</h1>
                 <h1 className='text-white font-medium text-3xl text-center mt-3'>Sushi Bar | Página Administrativa</h1>
                 
                 <main className='mt-10 flex flex-col md:flex-row md:flex-wrap md:justify-center gap-4'>
-                    <CardHome name="Platillos" comidas={"sushiii.jpg"} description="Total de Platillos: 20" conxt="Platillos no disponibles: 7" conxt2="Platillos disponibles: 13" index={1} ruta="/platillos"/>
-                    <CardHome name="Ordenes" comidas={"rest.png"} description="Total de Ordenes: 12" conxt="Ordenes completadas: 2" conxt2="Ordenes pendientes: 10" index={2} ruta="/ordenes"/>
-                    <CardHome name="Inventario" comidas={"camaron.jpg"} description="Total de Productos: 22  " conxt="Productos agotados: 10" conxt2="Productos disponibles: 12" index={3} ruta="/inventario"/>
+                    <CardHome name="Platillos" comidas={"sushiii.jpg"} description="Platillos" conxt={totalPlatillos} index={1} ruta="/platillos"/>
+                    <CardHome name="Órdenes" comidas={"rest.png"} description="Órdenes" conxt={totalOrdenes} index={2} ruta="/ordenes"/>
+                    <CardHome name="Inventario" comidas={"camaron.jpg"} description="Productos" conxt={totalProductos} index={3} ruta="/inventario"/>
                 </main>
 
             </div>
