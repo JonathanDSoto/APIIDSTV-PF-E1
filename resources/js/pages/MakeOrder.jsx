0eda367b041c95c31d907be540ed85993d0da773
Martín Castro <mcastro_20@alu.uabcs.mx>
2023-12-07 05:49:43 -0700

    Add: Eliminar registro modulo platillos

--- resources/js/pages/MakeOrder.jsx
+++ resources/js/pages/MakeOrder.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Form2 from '../components/Form2'
-import Card from '../components/Card'
-import Navbar from '../components/Navbar'
 import Layout from '../layouts/Layout'
 
 export default function MakeOrder() {
